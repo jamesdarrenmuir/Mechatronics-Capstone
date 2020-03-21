3d6095902a6aea127d1af238391cc55244492fd6
SometimesCalledJim <jim@sometimescalledjim.com>
2020-03-20 20:05:44 -0700

    added inner and outer loop code
    design and evaluation code

--- simulation.m
+++ simulation.m
@@ -1,13 +1,28 @@
 %% setup
 clear all; close all; clc;
-%% system parameters
-J_m = 0; % rotational inertia of the motor (kg-m^2)
-J_p1 = 0; % rotational inertia of the first pulley (kg-m^2)
-J_p2 = 0; % rotational inertia of the second pulley (kg-m^2)
-R_1 = 0; % radius of the first pulley (m)
-R_2 = 0; % radius of the second pulley (m)
-K_1 = 0; % spring constant of the first spring (N/m)
-K_2 = 0; % spring constant of the second spring (N/m)
-%%
+%% mechanical system parameters
+J_m = 0.001274; % rotational inertia of the motor and first pulley (kg-m^2)
+J_L = 0.01135; % rotational inertia of the second pulley and the load (kg-m^2)
+r_p = 4e-2; % radius of the pulleys (m)
+K = 300e3; % spring constant (N/m)
+%% set up base transfer function
 s = tf("s");
-Z_L = 0; % impedance of the load (some function of s)
+%% inner loop
+R_1 = r_p; R_2 = r_p; % pulley radii
+SEA = (K * R_1 * R_2)/(K * R_1^2 + J_m * s^2); % the plant (in this case the SEA device)
+inner_loop_controller = pidtune(SEA, "PID");
+%% evaluate inner loop controller
+closed_inner_loop = feedback(series(inner_loop_controller, SEA), 1);
+h = stepplot(closed_inner_loop);
+h.showCharacteristic('SettlingTime')
+h.showCharacteristic('PeakResponse')
+closed_inner_loop_info = stepinfo(closed_inner_loop)
+%% outer loop
+Z_L = 1/(J_L * s^2); % impedance of the load
+outer_loop_controller = pidTuner(Z_L, "PD");
+%% evaluate outer loop controller
+closed_outer_loop = feedback(series(outer_loop_controller, Z_L), 1);
+h = stepplot(closed_outer_loop);
+h.showCharacteristic('SettlingTime')
+h.showCharacteristic('PeakResponse')
+closed_outer_loop_info = stepinfo(closed_outer_loop)

1764a0a19e8313b781c23016f8082e9304ac206e
James Muir <jim@sometimescalledjim.com>
2020-04-13 15:43:30 -0700

    changed controller types, switched to pidtune
    changed inner loop controller to PIDF, changed outer loop controller to PDF,
    changed from using

--- design_controllers.m
+++ design_controllers.m
@@ -5,14 +5,17 @@ J_m = 0.001274; % rotational inertia of the motor and first pulley (kg-m^2)
 J_L = 0.01135; % rotational inertia of the second pulley and the load (kg-m^2)
 r_p = 4e-2; % radius of the pulleys (m)
 K = 300e3; % spring constant (N/m)
+K_vi = 0.41; % amplifier constant (A/V)
+K_t = 0.11; % motor torque constant (N-m/A)
 %% set up base transfer function
 s = tf("s");
 %% design inner loop controller
 R_1 = r_p; R_2 = r_p; % pulley radii
 SEA = (K * R_1 * R_2)/(K * R_1^2 + J_m * s^2); % the plant (in this case the SEA device)
-%inner_loop_controller = pidtune(SEA, "PID");
-pidTuner(SEA, "PID")
+inner_loop_plant = K_vi * K_t * SEA; % add the amplifier and torque constants
+inner_loop_controller = pidtune(inner_loop_plant, "PIDF");
+%pidTuner(inner_loop_transfer_function, "PIDF")
 %% design outer loop controller
 Z_L = 1/(J_L * s^2); % impedance of the load
-%outer_loop_controller = pidtune(Z_L, "PD");
-pidTuner(Z_L, "PD");
+outer_loop_controller = pidtune(Z_L, "PDF");
+%pidTuner(Z_L, "PD");

20a6f048e0432dea30c4f7fa6d82915d1bdf7896
James Muir <jim@sometimescalledjim.com>
2020-05-05 23:59:34 -0700

    changed names of variables
    removed references to multiple loops

--- evaluate_controllers.m
+++ evaluate_controllers.m
@@ -5,7 +5,7 @@ maximum_motor_output_torque = maximum_output_voltage * Kvi * Kt
 % output response
 figure('NumberTitle', 'off', 'Name', 'Output');
 closed_loop = feedback(series(controller, plant), 1);
-opt = stepDataOptions('StepAmplitude', pi/4);
+opt = stepDataOptions('StepAmplitude', pi/4); % 45 deg rotation step
 h = stepplot(closed_loop, opt);
 title("Output vs Time")
 ylabel("Position (rad)")
@@ -15,7 +15,7 @@ system_info = stepinfo(closed_loop)
 system_bandwidth = bandwidth(closed_loop)
 % effort response
 figure('NumberTitle', 'off', 'Name', 'Effort');
-inner_loop_controller_effort_transfer_function = feedback(tf(controller), plant);
-step(inner_loop_controller_effort_transfer_function, opt)
+controller_effort_transfer_function = feedback(tf(controller), plant);
+step(controller_effort_transfer_function, opt)
 title("Effort vs Time")
 ylabel("Voltage (V)")

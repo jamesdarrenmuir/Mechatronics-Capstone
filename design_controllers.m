635787144024a92088f371c9aa84eca395e36c26
James Muir <jim@sometimescalledjim.com>
2020-05-12 21:53:22 -0700

    Update design_controllers.m

--- design_controllers.m
+++ design_controllers.m
@@ -1,34 +1,27 @@
 %% setup
-% clear all; TODO: uncomment this
-close all; clc;
+clear all; close all; clc;
 %% mechanical system parameters
-%TODO: uncomment system parameters
-maximum_continuous_output_torque = 0.5; % (N-m) maximum target output torque
-
 %TODO: update these with the final values from the CAD model
 Jm = 2.14e-6; % (kg-m^2) moment of inertia of the motor
 Jg = 1.31e-7; % (kg-m^2) moment of inertia of the gearbox (as seen by the motor)
 Jp1 = 3.297e-6; % (kg-m^2) moment of inertia of the pulley closest to the motor
 Jp2 = Jp1; % (kg-m^2) moment of inertia of the pulley furthest from the motor
 
-% derive load
+JL = .007; % (kg-m^2) moment of inertia of the load
 
-% JL = .002; % (kg-m^2) moment of inertia of the load
-JL = J;
-
-% R1 = 2e-2; % (m) radius of the pulley closest to the motor
-R1 = r;
+R1 = 2e-2; % (m) radius of the pulley closest to the motor
 R2 = R1; % (m) radius of the pulley furthest from the motor
 
-% Rg = 16; % gearbox ratio, 16:1 speed reduction
+Rg = 16; % gearbox ratio, 16:1 speed reduction
 
 %TODO: update these values based on the selected springs
-% K1 = 1e8; % (N/m) spring constant of upper spring
-% K2 = K1; % (N/m) spring constant of lower spring
+K1 = 1e4; % (N/m) spring constant of upper spring
+K2 = K1; % (N/m) spring constant of lower spring
 
 %TODO: update this with values from the real amplifier
-% Kvi = 0.41; % (A/V) amplifier constant (from ME 477 lab)
-% Kt = 0.0214; % (N-m/A) motor torque constant
+Kvi = 0.41; % (A/V) amplifier constant (from ME 477 lab)
+Kt = 0.0214; % (N-m/A) motor torque constant
+maximum_continuous_output_torque = 0.5; % (N-m) maximum target output torque
 %TODO: update this if we use a different device
 maximum_output_voltage = 10; % (V) maximum output voltage of myRIO
 %% single loop controller
@@ -66,7 +59,7 @@ inner_loop_plant = Kvi * Kt * K*R1*R2*Rg/(K*R1^2+(Jp1+J1*Rg^2)*s^2);
 inner_loop_plant = minreal(inner_loop_plant);
 
 opt = pidtuneOptions("PhaseMargin", 60); % Default: 60 deg
-inner_loop_controller = pidtune(inner_loop_plant, "PIDF", 75, opt);
+inner_loop_controller = pidtune(inner_loop_plant, "PIDF", 150, opt);
 
 % evaluate inner loop controller
 % output response

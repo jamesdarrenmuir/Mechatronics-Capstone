b064d8b19c90483db603c71c7e2a2616a8815a4b
SometimesCalledJim <jim@sometimescalledjim.com>
2020-05-27 00:15:59 -0700

    Update main.c
    added Sramps function
    changed some variables to read from the controller.h file

--- C/Single Loop/main.c
+++ C/Single Loop/main.c
@@ -16,8 +16,9 @@
 #include "math.h"
 
 //#define M_PI	3.14159265358979323846
-#define Kvi		0.41		// Amplifier gain:  A/V
-#define Kt		0.11		// DC motor torque constant:  N-m/A
+// Kvi and Kt are now defined in single_loop_controller.h
+// #define Kvi		0.41		// Amplifier gain:  A/V
+// #define Kt		0.11		// DC motor torque constant:  N-m/A
 #define VDAmax	+7.5		// max D/A converter voltage: V
 #define VDAmin	-7.5		// min D/A converter voltage: V
 #define	ntot	5000
@@ -202,14 +203,100 @@ double	pos(MyRio_Encoder *channel, int *startP) {
 	position = (double)deltaP;			// BDI - displacement from starting position
 	return	position;
 }
+
+int  Sramps(seg *segs, int nseg, int *iseg, int *itime, double T, double *xa)
+{
+    // Computes the next position, *xa, of a uniform sampled position profile.
+    // The profile is composed of an array of segments (type: seg)
+    // Each segment consists of:
+    //      xfa:    final position
+    //      v:      maximum velocity
+    //      a:      maximum acceleration
+    //      d:      dwell time at the final position
+    //  Called from a loop, the profile proceeds from the current position,
+    //  through each segment in turn, and then repeats.
+    // Inputs:
+    //  seg *segs:  - segments array
+    //  int nseg:   - number of segments in the profile
+    //  int *iseg:  - variable hold segment index
+    //  int *itime  - time index within a segment (= -1 at segment beginning)
+    //  double T:   - time increment
+    // Outputs:
+    //  double *xa: - next position in profile
+    // Returns:     n - number of samples in the profile, 0 otherwise
+	//
+	//  Call with *itime = -1, *iseg = -1, outside the loop to initialize.
+
+    double t, t1=0, t2=1, tf=1, tramp, x1=1, xramp, xfr=1, xr, d;
+    static double x0, dir;
+    static int ntot;
+    double vmax=1, amax=1;
+    int n;
+
+    if (*itime==-1) {
+        (*iseg)++;
+        if(*iseg==nseg) {
+        	*iseg=0;
+        	ntot = 0;
+        }
+        *itime=0;
+        x0=*xa;
+    }
+    vmax=segs[*iseg].v;
+    amax=segs[*iseg].a;
+    d=segs[*iseg].d;
+    xfr=segs[*iseg].xfa-x0;
+    dir=1.0;
+    if(xfr<0){
+        dir=-1.;
+        xfr=-xfr;
+    }
+    t1 = vmax/amax;
+    x1 = 1./2.*amax*t1*t1;
+    if (x1<xfr/2) {
+        xramp = xfr-2.*x1;
+        tramp = xramp/vmax;
+        t2 = t1+tramp;
+        tf = t2+t1;
+    } else {
+        x1 = xfr/2;
+        t1 = sqrt(2*x1/amax);
+        t2 = t1;
+        tf = 2.*t1;
+    }
+    n = trunc((tf+d)/T)+1;
+
+    t = *itime*T;
+    if(t<t1) {
+        xr = 1./2.*amax*t*t;
+    } else if (t>=t1 && t<t2) {
+        xr = x1+vmax*(t-t1);
+    } else if (t>=t2 && t<tf) {
+        xr = xfr-1./2.*amax*(tf-t)*(tf-t);
+    } else {
+        xr = xfr;
+    }
+    *xa=x0+dir*xr;
+    (*itime)++;
+    if(*itime==n+1) {
+    	ntot = ntot + *itime - 1;
+        *itime=-1;
+        if(*iseg==nseg-1) {
+        	return ntot;
+        }
+    }
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
     int32_t status;
     MyRio_IrqTimer	irqTimer0;
     ThreadResource	irqThread0;
     pthread_t 		thread;
-    uint32_t 		timeoutValue;	/* time interval - us */
-    double			bti=0.5;
+	// timeoutValue is now defined in single_loop_controller.h
+    // uint32_t 		timeoutValue;	/* time interval - us */
+    // double			bti=0.5;
     double			vmax, amax, dwell;
     int				nseg;
 
@@ -238,7 +325,7 @@ int main(int argc, char **argv)
     /*  registers corresponding to the IRQ channel     */
     irqTimer0.timerWrite = IRQTIMERWRITE;
     irqTimer0.timerSet = IRQTIMERSETTIME;
-    timeoutValue = bti*1000.;
+    // timeoutValue = bti*1000.;
 
     /* Open the myRIO NiFpga Session. */
     status = MyRio_Open();

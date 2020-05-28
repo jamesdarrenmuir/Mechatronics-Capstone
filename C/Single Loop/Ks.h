9107ba90974d833aa4d990632703b3fb67945d00
James Muir <jim@sometimescalledjim.com>
2020-05-28 16:34:08 -0700

    updated header files

--- C/Single Loop/Ks.h
+++ C/Single Loop/Ks.h
@@ -1,4 +1,4 @@
 //---K values for SEA device
-//---28-May-2020 13:27:49
+//---28-May-2020 16:33:36
     double         Kt = 0.021400;              // motor torque constant (N-m/A)
     double         Kvi = 0.410000;              // amplifier constant (A/V)
--- C/Single Loop/single_loop_controller.h
+++ C/Single Loop/single_loop_controller.h
@@ -1,8 +1,8 @@
 //---Single loop PDF controller for SEA device
-//---28-May-2020 13:27:51
-    char        headerTime[] = "28-May-2020 13:27:51";
+//---28-May-2020 16:33:39
+    char        headerTime[] = "28-May-2020 16:33:39";
     int         single_loop_controller_ns = 1;              // number of sections
     uint32_t    timeoutValue = 5000;      // time interval - us; f_s = 200 Hz
     static	struct	biquad single_loop_controller[]={   // define the array of floating point biquads
-        {1.000000e+00, -9.986727e-01, 0.000000e+00, 1.000000e+00, -9.815128e-01, 0.000000e+00, 0, 0, 0, 0, 0}
+        {1.000000e+00, -9.981241e-01, 0.000000e+00, 1.000000e+00, -1.088331e-05, 0.000000e+00, 0, 0, 0, 0, 0}
         };

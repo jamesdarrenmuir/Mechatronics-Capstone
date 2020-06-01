da9eb147ae58856549bd48df21d92b73bf55f575
James Muir <jim@sometimescalledjim.com>
2020-05-31 21:13:50 -0700

    Create conC_Encoder_initialize.h

---
+++ C/Real Controllers/conC_Encoder_initialize.h
@@ -0,0 +1,14 @@
+//
+//  conC_Encoder_initialize.h
+//  
+//
+//  Created by JOSEPH L GARBINI on 12/28/17.
+//
+
+#ifndef conC_Encoder_initialize_h
+#define conC_Encoder_initialize_h
+
+#include <stdio.h>
+#include "Encoder.h"
+
+#endif /* conC_Encoder_initialize_h */

bdca16e2c54d97c8225f378e7389e8968af1d8d1
James Muir <jim@sometimescalledjim.com>
2020-06-01 16:25:48 -0700

    deduplicated graphing code
    created evaluate_controller graphing function

--- MATLAB/Real Controllers/design_controllers.m
+++ MATLAB/Real Controllers/design_controllers.m
@@ -11,21 +11,23 @@ Jm = 2.14e-6; % (kg-m^2) moment of inertia of the motor
 
 % gearbox
 Rg = 16; % gearbox ratio, 16:1 speed reduction
-Jg = 1.31e-7; % (kg-m^2) moment of inertia of the gearbox (as seen by the motor)
+% as seen by the motor
+Jg = 1.31e-7; % (kg-m^2) moment of inertia of the gearbox
 
 % shaft couplers
 Js = 8.6e-7; % (kg-m^2) moment of inertia of the shaft couplers
 
 % encoder
-BDI_per_rev = 500 * 4; % (BDI/rev) 500 windows * 4 ticks per window for a quadrature encoder
+% 500 windows * 4 ticks per window for a quadrature encoder
+BDI_per_rev = 500 * 4; % (BDI/rev) 
 
 % pulleys
-% pulley 1
+% pulley 1 (closest to the motor)
 R1 = 2e-2; % (m) radius of the pulley closest to the motor
-Jp1 = 2.846e-6; % (kg-m^2) moment of inertia of the pulley closest to the motor
-% pulley 2
+Jp1 = 2.846e-6; % (kg-m^2) moment of inertia of the pulley 
+% pulley 2 (furthest from the motor)
 R2 = R1; % (m) radius of the pulley furthest from the motor
-Jp2 = Jp1; % (kg-m^2) moment of inertia of the pulley furthest from the motor
+Jp2 = Jp1; % (kg-m^2) moment of inertia of the pulley
 
 % load
 JL = .00101; % (kg-m^2) moment of inertia of the load
@@ -45,35 +47,31 @@ K = K1 + K2;
 J1 = Jm + Jg + Js;
 J2 = Jp2 + JL;
 %% single loop controller
-
+name = 'Single Loop';
 % set up plant
-single_loop_plant = Kvi * Kt * K*R1*R2*Rg/(J2*(Rg^2*J1+Jp1)*s^4+K*(Rg^2*R2^2*J1+Jp1*R2^2+J2*R1^2)*s^2);
+single_loop_plant = Kvi * Kt * K*R1*R2*Rg ... 
+    /(J2*(Rg^2*J1+Jp1)*s^4+K*(Rg^2*R2^2*J1+Jp1*R2^2+J2*R1^2)*s^2);
 single_loop_plant = minreal(single_loop_plant);
 
 % design controller
 opt = pidtuneOptions("PhaseMargin", 60); % Default: 60 deg
 single_loop_controller = pidtune(single_loop_plant, "PDF", 1, opt);
 
-% evaluate single loop controller
-% output response
-figure('NumberTitle', 'off', 'Name', 'Single Loop Output');
-closed_loop = feedback(series(single_loop_controller, single_loop_plant), 1);
-opt = stepDataOptions('StepAmplitude', pi/4); % 45 deg rotation step
-h = stepplot(closed_loop, opt);
-xlim([0 10])
-title("Output vs Time")
-ylabel("Position (rad)")
-h.showCharacteristic('SettlingTime')
-h.showCharacteristic('PeakResponse')
-% single_loop_info = stepinfo(closed_loop)
-% single_loop_bandwidth = bandwidth(closed_loop)
+% design lead controller
+% 10% OS -> zeta = 0.6
+% zeta = .6;
+% Ts = 1; % (s)
+% 
+% wn = 4 / (Ts * zeta); % (rad/s)
+% zc = 80;
+% 
+% [~, ~, single_loop_controller] = design_lead_compensator(zeta, Ts, ...
+%     zc, single_loop_plant, name);
 
-% effort response
-figure('NumberTitle', 'off', 'Name', 'Single Loop Effort');
-controller_effort_transfer_function = feedback(tf(single_loop_controller), single_loop_plant);
-step(controller_effort_transfer_function, opt)
-title("Effort vs Time")
-ylabel("Voltage (V)")
+% evaluate single loop controller
+% 45 deg step
+evaluate_controller(name, single_loop_controller, ...
+    single_loop_plant, pi/4, 1000, "Position (rad)", "Voltage (V)");
 
 % discretize controller
 T = 0.005; %(s)
@@ -86,6 +84,7 @@ slc2header(fileID, slc, T, Kvi, Kt, BDI_per_rev);
 %% double loop controllers
 % the inner loop is nested inside the outer loop
 %% inner loop controller
+name = 'Inner Loop';
 % set up plant
 inner_loop_plant = Kvi * Kt * K*R1*R2*Rg/(K*R1^2+(Jp1+J1*Rg^2)*s^2);
 inner_loop_plant = minreal(inner_loop_plant);
@@ -98,39 +97,21 @@ Ts = 0.03; % (s) Garbini's recommendation
 % Ts = 0.08;
 wn = 4 / (Ts * zeta); % (rad/s)
 
-z = 80; % Garbini's recommendation
-% z = 25;
+% z = 80; % Garbini's recommendation
+zc = 120;
 
-[~, ~, inner_loop_controller] = design_lead_compensator(zeta, Ts, z, inner_loop_plant, 'Inner Loop');
+[~, ~, inner_loop_controller] = design_lead_compensator(zeta, Ts, ...
+    zc, inner_loop_plant, name);
 
 % evaluate inner loop controller
-% output response
-figure('NumberTitle', 'off', 'Name', 'Inner Loop Output');
-closed_loop = feedback(series(inner_loop_controller, inner_loop_plant), 1);
-maximum_motor_output_torque = maximum_output_voltage * Kvi * Kt;
-opt = stepDataOptions('StepAmplitude', MCOT);
-h = stepplot(closed_loop, opt);
-hold on
-y = yline(MCOT);
-ylim([-1.1*MCOT 1.1*MCOT])
-legend(y, 'Target')
-title("Output vs Time")
-ylabel("Torque (N-m)")
-h.showCharacteristic('SettlingTime')
-h.showCharacteristic('PeakResponse')
-% inner_loop_info = stepinfo(closed_loop)
-% inner_loop_bandwidth = bandwidth(closed_loop)
-
-% effort response
-figure('NumberTitle', 'off', 'Name', 'Inner Loop Effort');
-controller_effort_transfer_function = feedback(tf(inner_loop_controller), inner_loop_plant);
-step(controller_effort_transfer_function, opt)
-title("Effort vs Time")
-ylabel("Voltage (V)")
+evaluate_controller(name, inner_loop_controller, ...
+    inner_loop_plant, MCOT, .1, "Torque (N-m)", "Voltage (V)");
 %% outer loop controller
+name = 'Outer Loop';
 % set up plant
 Z2 = 1/(J2*s^2);
-outer_loop_plant = series(feedback(series(inner_loop_controller, inner_loop_plant), 1), Z2);
+outer_loop_plant = series(feedback(series(inner_loop_controller, ...
+    inner_loop_plant), 1), Z2);
 
 % design controller
 % output torque < 0.5 N-m
@@ -138,24 +119,9 @@ opt = pidtuneOptions("PhaseMargin", 60); % Default: 60 deg
 outer_loop_controller = pidtune(outer_loop_plant, "PIDF", 4, opt);
 
 % evaluate outer loop controller
-% output response
-figure('NumberTitle', 'off', 'Name', 'Outer Loop Output');
-closed_loop = feedback(series(outer_loop_controller, outer_loop_plant), 1);
-opt = stepDataOptions('StepAmplitude', pi/4); % 45 deg rotation step
-h = stepplot(closed_loop, opt);
-title("Output vs Time")
-ylabel("Position (rad)")
-h.showCharacteristic('SettlingTime')
-h.showCharacteristic('PeakResponse')
-% outer_loop_info = stepinfo(closed_loop)
-% outer_loop_bandwidth = bandwidth(closed_loop)
-
-% effort response
-figure('NumberTitle', 'off', 'Name', 'Outer Loop Effort');
-controller_effort_transfer_function = feedback(tf(outer_loop_controller), outer_loop_plant);
-step(controller_effort_transfer_function, opt)
-title("Effort vs Time")
-ylabel("Torque (N-m)")
+% 45 deg step
+evaluate_controller(name, outer_loop_controller, ...
+    outer_loop_plant, pi/4, 10, "Position (rad)", "Torque (N-m)")
 %% save double loop controller as .h file
 % discretize inner loop controller
 T = 0.005; %(s)
@@ -201,4 +167,30 @@ function [pc, Kc, controller] = design_lead_compensator(zeta, Ts, zc, plant, nam
                         'MarkerFaceColor','g',...
                         'MarkerSize',9);
     legend([h1 h2] , ["closed loop poles" "target poles"])
+end
+
+function [info, bw] = evaluate_controller(name, controller, plant, stepAmp, time, outputLabel, effortLabel)
+    % plots graphs with information about a controller
+    
+    % output response
+    figure('NumberTitle', 'off', 'Name', append(name, ' Output'));
+    closed_loop = feedback(series(controller, plant), 1);
+    opt = stepDataOptions('StepAmplitude', stepAmp); % 45 deg rotation step
+    h = stepplot(closed_loop, time, opt);
+    hold on
+    y = yline(stepAmp);
+    legend(y, 'Target')
+    title("Output vs Time")
+    ylabel(outputLabel)
+    h.showCharacteristic('SettlingTime')
+    h.showCharacteristic('PeakResponse')
+    info = stepinfo(closed_loop);
+    bw = bandwidth(closed_loop);
+
+    % effort response
+    figure('NumberTitle', 'off', 'Name', append(name, ' Effort'));
+    controller_effort_transfer_function = feedback(tf(controller), plant);
+    step(controller_effort_transfer_function, opt)
+    title("Effort vs Time")
+    ylabel(effortLabel)
 end

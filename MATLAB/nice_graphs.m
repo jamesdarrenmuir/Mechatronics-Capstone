a04332c47cad63baf44ada54e4cabd614b059a0f
James Muir <jim@sometimescalledjim.com>
2020-06-11 21:07:21 -0700

    created some nice data visualizations
    make some nice figures using data we collected. data is stored in Google Drive and must be downloaded and their folder added to the MATLAB path

---
+++ MATLAB/nice_graphs.m
@@ -0,0 +1,70 @@
+clearvars; close all; clc;
+%% Inner Loop
+% inner loop reference tracking
+load("InnerLoop.mat")
+figure('NumberTitle', 'off', 'Name', "Inner Loop Reference Tracking");
+plot(time, reference_spring_torque, 'DisplayName', 'Reference Spring Torque');
+hold on
+plot(time, actual_spring_torque, 'DisplayName', 'Actual Spring Torque');
+legend()
+ylim([-.3 .3])
+xlim([time(1) time(end)])
+xlabel("Time (s)")
+ylabel("Torque (N-m)")
+title("Time vs Torque")
+%% Double Loop Disturbance
+% double loop disturbance rejection
+clearvars;
+load("DoubleLoopDisturbance.mat")
+figure('NumberTitle', 'off', 'Name', "Double Loop Disturbance Rejection");
+n = 2; m = 1;
+% position
+subplot(n, m, 1);
+plot(time, reference_position, 'DisplayName', 'Reference Position');
+hold on
+plot(time, actual_position, 'DisplayName', 'Actual Position');
+legend()
+% ylim([-.3 .3])
+xlim([time(1) time(end)])
+xlabel("Time (s)")
+ylabel("Position (rad)")
+title("Time vs Position")
+% torque
+subplot(n, m, 2);
+plot(time, reference_spring_torque, 'DisplayName', 'Reference Spring Torque');
+hold on
+plot(time, actual_spring_torque, 'DisplayName', 'Actual Spring Torque');
+legend()
+% ylim([-.3 .3])
+xlim([time(1) time(end)])
+xlabel("Time (s)")
+ylabel("Torque (N-m)")
+title("Time vs Torque")
+%% Double Loop Reference
+% double loop reference tracking
+clearvars;
+load("DoubleLoopReference.mat")
+figure('NumberTitle', 'off', 'Name', "Double Loop Reference Tracking");
+n = 2; m = 1;
+% position
+subplot(n, m, 1);
+plot(time, reference_position, 'DisplayName', 'Reference Position');
+hold on
+plot(time, actual_position, 'DisplayName', 'Actual Position');
+legend()
+% ylim([-.3 .3])
+xlim([time(1) time(end)])
+xlabel("Time (s)")
+ylabel("Position (rad)")
+title("Time vs Position")
+% torque
+subplot(n, m, 2);
+plot(time, reference_spring_torque, 'DisplayName', 'Reference Spring Torque');
+hold on
+plot(time, actual_spring_torque, 'DisplayName', 'Actual Spring Torque');
+legend()
+% ylim([-.3 .3])
+xlim([time(1) time(end)])
+xlabel("Time (s)")
+ylabel("Torque (N-m)")
+title("Time vs Torque")
